3352d57eb212353e3d622332f28e5cef1d43294c
anyanikolskaya2111-art <anyanikolskaya21.11@gmail.com>
2026-06-04 16:40:03 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,4 +9,8 @@ def multiply(a, b):
 HEAD
 def rebase_func(): pass
 def divide(a, b): return a / b if b != 0 else None
-13cb54a (dev2: add divide function)
+
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
